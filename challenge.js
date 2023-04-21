f0638975a1f3183d00027b269bd6b1505fb97a94
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-21 16:10:39 -0500

    Update challenge.js
    Defined functions for the challenge

--- challenge.js
+++ challenge.js
@@ -1,3 +1,23 @@
+class Node {
+  constructor(value, next_node = null) {
+    this.value = value;
+    this.next_node = next_node;
+  }
+}
+
+class LinkedList {
+  // setup head and tail
+
+  add(number) {
+    // your code here  
+    }
+  
+
+  get(index) {
+    // your code here
+    }
+  }
+
 
 const list = new LinkedList();
 
