15b5743d302ca855f2fe9fe6695caa3741b96d13
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-21 16:11:26 -0500

    Update challenge.js
    Final Solution!

--- challenge.js
+++ challenge.js
@@ -7,14 +7,34 @@ class Node {
 
 class LinkedList {
   // setup head and tail
+  constructor(){
+    this.head = null;
+    this.tail = null;
+  }
 
   add(number) {
-    // your code here  
+    // your code here
+    const node = new Node(number);
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next_node = node;
+      this.tail = node;
+    }   
     }
   
 
   get(index) {
     // your code here
+    if(index < 0 || index >= this.length) return null;
+        var counter = 0;
+        var current = this.head;
+        while(counter !== index){
+            current = current.next_node;
+            counter++;
+        }
+        return current.value;
     }
   }
 
